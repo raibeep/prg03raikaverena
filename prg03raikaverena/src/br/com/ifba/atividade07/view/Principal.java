9706c9d0cddb818e837db98bb2c7f77ba13f5be1
raibeep <20242ireads0002@ifba.edu.br>
2025-10-11 22:30:57 -0300

    modificações no main para modelo interativo
    cria a conta

--- prg03raikaverena/src/br/com/ifba/atividade07/view/Principal.java
+++ prg03raikaverena/src/br/com/ifba/atividade07/view/Principal.java
@@ -3,14 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.ifba.atividade07.view;
-
+import java.util.Scanner;
 /**
  *
  * @author Raika
  */
 public class Principal {
     public static void main(String[] args){
-        Banco cliente1 = new Banco ("Jubileu", 1111);
-        cliente1.abrirConta("cc");
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("== ABERTURA DE CONTA ==");
+        System.out.println("Nome do cliente: ");
+        String nome = entrada.nextLine();//lê o que foi digitado
+        
+        System.out.println("Número da conta");
+        int numConta = entrada.nextInt();// lê o que foi digitado
+        entrada.nextLine(); //limpa buffer
+        
+        Banco conta = new Banco(nome, numConta);//inicializa a conta
+        
+        System.out.println("Tipo da conta (cc ou cp): ");
+        String tipo = entrada.nextLine();//lê o que foi digitado
+        
+        conta.abrirConta(tipo);//abre a conta
+        
     }
 }
