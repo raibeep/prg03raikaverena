1610c47482ca486f84e7d180556912f7b27b957f
raibeep <20242ireads0002@ifba.edu.br>
2025-10-25 16:15:25 -0300

    cria classe forma

---
+++ prg03raikaverena/src/br/com/ifba/atividade10/view/Forma.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade10.view;
+
+/**
+ *
+ * @author raiii
+ */
+public abstract class Forma {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
